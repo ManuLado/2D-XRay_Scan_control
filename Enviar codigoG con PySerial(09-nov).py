f783c166a24c7f5156a282c0fefc8581d43e4a8c
ManuLado <77543157+ManuLado@users.noreply.github.com>
2021-01-18 14:09:33 -0300

    ahu hay que hacer andar la toma de codigo g  por consola

--- Enviar codigoG con PySerial(09-nov).py
+++ Enviar codigoG con PySerial(09-nov).py
@@ -7,6 +7,8 @@ import time
 from datetime import date
 import datetime
 import os
+import sys
+
 #/////////////////////////////////
 if not os.path.exists('logdir.txt'):
     with open('logdir.txt','w') as f:
@@ -40,22 +42,39 @@ arduino.write(b"G92 X0 Y0\n")          #set current position as 00 (calibrar en 
 arduino.flush()
 #................................................................................................
 #definicion de variables e input
-xrange=float(input("ingrese ancho x de muestreo [mm]"))#200
-yrange=float(input("ingrese largo y de muestreo[mm]"))#50
-pasos=float(input("ingrese pasos de muestreo (numero de imagenes):"))#10
+gcode=str(input("<<<<ingrese codigo g>>>>:"))
+arduino.write(bytes(gcode,'utf-8'))
+
+inp=input("<<<<CONTINUAR. presione cualquier tecla("'exit'" para salir)>>>>>")
+if inp=="exit" or inp=="EXIT":
+    sys.exit()
+    
+
+xrange=float(input("<<<<ingrese ancho x de muestreo [mm]>>>>"))#200
+yrange=float(input("<<<<ingrese largo y de muestreo[mm]>>>>"))#50
+pasos=float(input("<<<<ingrese pasos de muestreo (numero de imagenes)>>>>:"))#10
 
 xstep=int(xrange/pasos)
 ystep=int(yrange/pasos)
 movx='G0 X'+str(xstep) + " \\" +'n'
-movy='G0 Y'+str(ystep) + " \\" +'n'
+movy="G0 Y"+str(ystep) + " \\" +'n'
+rnx='G0 X'+str(int(xrange)) + " \\" +'n'
+rny='G0 X0 Y'+str(int(yrange)) + " \\" +'n'
 print(movx)
 
+arduino.flush()
+arduino.write(bytes(movy,'utf-8'))
+#arduino.write(b"G0 Y-50\n")
+arduino.flush()
+a=arduino.readline()
+print("arduino dice:",a)
 
 
 #................................................................................................
 #bucles while, zigzag
 i=0
-arduino.write(b"G0 X-200 Y-50\n")
+arduino.write(b"G0 Y-50\n")
+
 time.sleep(10)
 arduino.flush()
 while i<pasos:
@@ -63,8 +82,10 @@ while i<pasos:
     i+=1
     while j<pasos:
         j+=1
-        #arduino.write(bytes(movx,'utf-8'))
-        arduino.write(b"G0 X30 \n") 
+        arduino.write(bytes(movx,'utf-8'))
+        #arduino.write(b"G0 X30 \n")
+        a=arduino.readline()
+        print("arduino dice:",a)
         arduino.flush()
         arduino.write(b" M114 \n")
         arduino.flush()
@@ -97,11 +118,12 @@ while i<pasos:
             break
     arduino.flush()    
     #arduino.write(b"G28 X \n") #regresa a x=0
-    arduino.write(b"G0 X-200 \n")
+    #arduino.write(b"G0 X-200 \n")
+    arduino.write(bytes(movx,'utf-8'))
     arduino.flush()
     time.sleep(10)
-    #arduino.write(bytes(movy,'utf-8'))
-    arduino.write(b"G0 Y5 \n")
+    arduino.write(bytes(movy,'utf-8'))
+    #arduino.write(b"G0 Y5 \n")
 ##
 #output=arduino.read(5)
 #print(str(output,'utf8'))
