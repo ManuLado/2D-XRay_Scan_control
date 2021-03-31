1feef7078974bde80d46c1132ecbbdd287b8a1f8
ManuLado <77543157+ManuLado@users.noreply.github.com>
2021-03-31 14:25:38 -0300

    Add files via upload

---
+++ fits2jpeg.py
@@ -0,0 +1,23 @@
+#este script transforma las imagenes tomadas en formato .fits a .jpeg o .png
+#se pueden cambiar varios parametros de la imagen, filtros, tamanio,etc (ver documentacion de ds9)
+#requiere tener instalado SAOImage DS9
+
+import os
+
+mylist=os.listdir('date_2021-03-31_run_46') #modify to change directory dynamically
+print("number of files:",len(mylist))
+print(mylist)
+
+input("Enter to continue..")
+for i in mylist:
+    string1=str(i)
+    
+    str2=string1.replace('fits','png') #aca cambiar tambien el formato elejido
+    #comandos ds9:
+        #-cd> set working directory
+        #-file> open .fits file
+        #-cmap> mapa de colores (Heat, h5_bone,gray,etc)
+        #-export> guardar imagen (png,jpeg,etc)
+    os.system("ds9 -cd date_2021-03-31_run_46 -file "+string1+" -cmap h5_bone -export png "+str2+' -exit')
+    
+    
